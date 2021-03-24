d68e802d6003c0ccf2c926f7aa51115841a55c9d
AleksandraL99 <lejaaleksandra9@gmail.com>
2021-03-24 12:19:02 +0100

    Initial commit

---
+++ src/test/java/pl/roligt/roligt/RoligtApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.roligt.roligt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoligtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
