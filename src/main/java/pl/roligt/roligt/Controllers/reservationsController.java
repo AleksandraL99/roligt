66baeda127b3eecbc7b7473c071fb70f763e6d9f
AleksandraL99 <lejaaleksandra9@gmail.com>
2021-04-10 10:58:03 +0200

    Added Controllers and basic access rights

---
+++ src/main/java/pl/roligt/roligt/Controllers/reservationsController.java
@@ -0,0 +1,19 @@
+package pl.roligt.roligt.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class reservationsController {
+
+    @GetMapping("/reservations")
+    public String getReservations() {
+        return "reservations";
+    }
+
+    @GetMapping("/resadmin")
+    public String getAdminReservations() {
+        return "resadmin";
+    }
+
+}
---
+++ src/main/java/pl/roligt/roligt/UserRepo.java
@@ -0,0 +1,9 @@
+package pl.roligt.roligt;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<UsersTable, Long> {
+}
+
