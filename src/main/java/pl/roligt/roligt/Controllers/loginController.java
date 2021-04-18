a0f2634268c2e20e41a1cbaee5919f603850d9e0
AleksandraL99 <lejaaleksandra9@gmail.com>
2021-04-18 15:21:08 +0200

    Corrected reservation page from admin, added thymelead for Controller

--- src/main/java/pl/roligt/roligt/Controllers/loginController.java
+++ src/main/java/pl/roligt/roligt/Controllers/loginController.java
@@ -4,12 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class reservationsController {
-
-    @GetMapping("/reservations")
-    public String getReservations() {
-        return "reservations";
+public class loginController {
+    @GetMapping("/loginpage")
+    public String getLogin() {
+        return "loginPage";
     }
-
-
 }
